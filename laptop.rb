73b14ca9025527df25338cadfe876251ca0bfc82
Tim Riley <tim@openmonkey.com>
2014-10-24 08:15:04 +1100

    Make my own “workstation” dep to wrap up my personal changes

--- laptop.rb
+++ laptop.rb
@@ -2,5 +2,9 @@ dep "laptop" do
   # Build the standard Icelab environment
   requires "icelab:workstation"
 
+  requires "workstation"
+end
+
+dep "workstation" do
   requires "personal dotfiles"
 end
