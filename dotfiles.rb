f574acd408367f0b8ee32c38821f870e6ce14ccc
Tim Riley <tim@openmonkey.com>
2014-10-24 12:26:51 +1100

    Use 1.8 hash syntax

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,8 +1,8 @@
 dep "personal dotfiles" do
   requires "icelab:dotfiles"
-  requires "icelab:repository".with  path: "~/.dotfiles-personal",
-                              url: "https://github.com/timriley/dotfiles.git",
-                              branch: "master"
+  requires "icelab:repository".with :path => "~/.dotfiles-personal",
+                                    :url => "https://github.com/timriley/dotfiles.git",
+                                    :branch => "master"
 
   met? {
     "~/.dotfiles-personal".p.exists?
