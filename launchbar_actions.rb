b4757efb65961fa134f912567b53a3712bea45ca
Tim Riley <tim@openmonkey.com>
2015-06-19 21:39:58 +1000

    Properly extract lbactions when installing them
    This makes the dep template actually work.

--- launchbar_actions.rb
+++ launchbar_actions.rb
@@ -1,3 +1,21 @@
+# Patch babushka's `Resource.extract` so it doesn't descend into the top level
+# .lbaction directory automatically after extracting the archives.
+#
+# Remove once https://github.com/benhoskings/babushka/issues/326 is fixed.
+module Babushka
+  class Asset
+    def content_subdir
+      identity_dirs.reject {|dir|
+        %w[app pkg bundle tmbundle prefPane lbaction].map {|i| # added `lbaction` here
+          /\.#{i}$/
+        }.any? {|dont_descend|
+          dir[dont_descend]
+        }
+      }.first
+    end
+  end
+end
+
 meta :lbaction do
   accepts_value_for :source
 
@@ -11,7 +29,9 @@ meta :lbaction do
     }
 
     meet {
-      Babushka::Resource.download source, path
+      Babushka::Resource.extract(source) do |archive|
+        Dir.glob("*.lbaction").first.p.copy path
+      end
     }
   }
 end
