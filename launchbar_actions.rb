976b082ed5ffedb5fe0f75375d86420f54ada939
Tim Riley <tim@openmonkey.com>
2017-07-22 08:30:33 +1000

    Remove Babushka monkeypatch
    This change has been moved upstream now

--- launchbar_actions.rb
+++ launchbar_actions.rb
@@ -1,21 +1,3 @@
-# Patch babushka's `Resource.extract` so it doesn't descend into the top level
-# .lbaction directory automatically after extracting the archives.
-#
-# Remove once https://github.com/benhoskings/babushka/issues/326 is fixed.
-module Babushka
-  class Asset
-    def content_subdir
-      identity_dirs.reject {|dir|
-        %w[app pkg bundle tmbundle prefPane lbaction].map {|i| # added `lbaction` here
-          /\.#{i}$/
-        }.any? {|dont_descend|
-          dir[dont_descend]
-        }
-      }.first
-    end
-  end
-end
-
 meta :lbaction do
   accepts_value_for :source
 
