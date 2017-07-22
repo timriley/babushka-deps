8e40b41eb5bce244d56f138a6cebb79f67ef9d72
Tim Riley <tim@openmonkey.com>
2017-07-22 10:26:01 +1000

    Add a ‘laptop bootstrapped’ dep
    I need this to get the latest rcm installed before ‘icelab:workstation bootstrapped’ installs the Icelab dotfiles

--- laptop.rb
+++ laptop.rb
@@ -1,8 +1,12 @@
-dep "laptop" do
+dep "laptop bootstrapped" do
   # Install rcm from source first, to get a version with fixes for file names
   # with spaces (necessary until a version _after_ 1.3.1 is released).
   requires "rcm.src"
 
+  requires "icelab:workstation bootstrapped"
+end
+
+dep "laptop" do
   # Build the standard Icelab environment
   requires "icelab:workstation"
 
